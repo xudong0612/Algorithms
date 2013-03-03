e6e7f8484def9689f64723353e60513c39143771
xudong0612 <xudong0612@gmail.com>
2013-03-03 16:42:03 +0800

    Add Queue superclass

---
+++ Algorithms/src/QueueOfStrings.java
@@ -0,0 +1,20 @@
+
+public class QueueOfStrings {
+	public QueueOfStrings() {
+	}
+
+	public void enqueue(String item) {
+	}
+
+	public String dequeue() {
+		return null;
+	}
+
+	public boolean isEmpty() {
+		return true;
+	}
+
+	public int size() {
+		return -1;
+	}
+}
